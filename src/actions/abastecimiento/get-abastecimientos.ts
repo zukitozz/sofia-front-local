017c360b4703cf39892d9677a84f248f6ce50bbf
xt9052 <xt9052@rimac.com.pe>
2026-06-18 20:04:04 -0500

    capturando abastecimientos 0 pero con contador con valores

--- src/actions/abastecimiento/get-abastecimientos.ts
+++ src/actions/abastecimiento/get-abastecimientos.ts
@@ -8,8 +8,10 @@ export async function getAbastecimientos(pistolas: number[], estado?: EstadosAba
         const condiciones: string[] = [];
 
         // Filtro por estado (mantenemos tu lógica de valorTotal > 0 cuando hay estado)
+        condiciones.push(`a.fechaHora >= DATEADD(day, -2, GETDATE()) AND (a.valorTotal > 0 OR (a.valorTotal = 0 AND a.totInicio <> a.totFinal))`); // Solo los abastecimientos de los últimos 2 días
+
         if (estado !== undefined) {
-            condiciones.push(`a.valorTotal > 0 AND a.estado = ${estado}`);
+            condiciones.push(`a.estado = ${estado}`);
         }
 
         // 2. Filtro por pistolas: SOLO se agrega si el array contiene elementos
@@ -24,10 +26,19 @@ export async function getAbastecimientos(pistolas: number[], estado?: EstadosAba
 
         // 4. Construir la query final de forma mucho más legible
         const query = `
-            SELECT 
-                a.idAbastecimiento, a.registro, a.pistola, a.codigoCombustible, a.numeroTanque, a.valorTotal, a.volTotal, a.precioUnitario, 
-                a.tiempo, a.fechaHora, a.totInicio, a.totFinal, a.IDoperador, a.IDcliente, a.volTanque, a.estado, 
-                p.nombre, p.color, p.medida  
+            SELECT TOP 50 
+                a.idAbastecimiento, a.registro, a.pistola, a.codigoCombustible, a.numeroTanque, 
+                CASE 
+                    WHEN a.valorTotal = 0 AND a.totInicio <> a.totFinal THEN ROUND((a.totFinal - a.totInicio) * a.precioUnitario, 2)
+                    ELSE a.valorTotal 
+                END AS valorTotal,
+                CASE 
+                    WHEN a.volTotal = 0 AND a.totInicio <> a.totFinal THEN ROUND((a.totFinal - a.totInicio), 3)
+                    ELSE a.volTotal 
+                END AS volTotal,
+                a.precioUnitario,
+                a.tiempo, a.fechaHora, a.totInicio, a.totFinal, a.IDoperador, a.IDcliente, a.volTanque, a.estado,
+                p.nombre, p.color, p.medida 
             FROM 
                 DEMOSQL.dbo.Abastecimientos a 
             INNER JOIN 
